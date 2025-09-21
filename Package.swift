b4b002ddb290b40603711a08b477dd97fb1c25d9
Marc Haisenko <haisenko@equinux.com>
2025-09-21 11:53:52 +0200

    Experiment with traits

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.1
 
 import PackageDescription
 
@@ -8,6 +8,11 @@ let package = Package(
     products: [
         .library(name: "SwiftSoup", targets: ["SwiftSoup"])
     ],
+    traits: [
+        "LRUCache",
+        "Atomics",
+        .default(enabledTraits: ["LRUCache", "Atomics"])
+    ],
     dependencies: [
         .package(url: "https://github.com/nicklockwood/LRUCache.git", from: "1.1.2"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.3.0"),
@@ -16,15 +21,15 @@ let package = Package(
         .target(
             name: "SwiftSoup",
             dependencies: [
-                .product(name: "LRUCache", package: "LRUCache"),
-                .product(name: "Atomics", package: "swift-atomics")
+                .product(name: "LRUCache", package: "LRUCache", condition: .when(traits: ["LRUCache"])),
+                .product(name: "Atomics", package: "swift-atomics", condition: .when(traits: ["Atomics"]))
             ],
             path: "Sources"),
         .testTarget(
             name: "SwiftSoupTests", 
             dependencies: [
                 "SwiftSoup",
-                .product(name: "Atomics", package: "swift-atomics")
+                .product(name: "Atomics", package: "swift-atomics", condition: .when(traits: ["Atomics"]))
             ])
     ]
 )
